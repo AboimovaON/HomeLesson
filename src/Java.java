cca3676b7ae2931047bbb9e488e3b376ed2ac998
AboimovaON <85744610+AboimovaON@users.noreply.github.com>
2021-07-06 10:47:59 +0500

    Initial commit

---
+++ src/Java.java
@@ -0,0 +1,10 @@
+public class Java {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("Однажды в студенную зимнюю пору");
+        System.out.println("Я из лесу вышел");
+        System.out.println("Был сильный мороз");
+        System.out.println("смотрю");
+
+    }
+    }
