7a74a403963ffb1f977fde6f3f7e7bee076ad2a3
AboimovaON <85744610+AboimovaON@users.noreply.github.com>
2021-07-16 21:23:50 +0500

    Домашняя работа по четвертому заданию выполнена

---
+++ src/WorkHome/java/Cat.java
@@ -0,0 +1,15 @@
+package WorkHome.java;
+
+public class Cat extends Animal
+    {
+            public Cat(float maxRunDistance)
+            {
+                super(maxRunDistance, Integer.MIN_VALUE);
+            }
+
+            @Override
+            public void swimOn(float requiredDistance)
+            {
+                System.out.println("Кошки плавать не умеют!\n");
+            }
+        }
